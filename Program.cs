a288adf466388f423c11cfd8dd8b5ae47cba96b3
Nauryzbai <nauzik@mail.ru>
2019-04-04 00:20:23 +0600

    Practice13

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Xml;
+using System.Xml.Linq;
+
+namespace Practice13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] xmlArray = {"Name","Surname","Age","Education", "Status" };
+            XDocument xml = XDocument.Load("NewXmlFile.xml");
+            int i = 0;
+            var hello = from body in xml.Descendants("body")
+                        select new
+                        {
+                            text = body.Element(xmlArray[i]).Value,
+                        };
+            while (i < xmlArray.Length)
+            {
+                foreach (var body in hello)
+                {
+                    Console.Write(body.text + " ");
+                }
+                i++;
+            }
+            xml.Save("NewXmlFile.xml");
+            Console.ReadLine();
+        }
+    }
+    }
